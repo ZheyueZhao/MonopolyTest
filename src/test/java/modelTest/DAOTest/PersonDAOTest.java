0b389114cf14754b99535f8be65e1f24024c747f
ZheyueZhao <zheyuezhao@gmail.com>
2024-04-16 10:48:09 -0400

    finish personDAO test

--- src/test/java/modelTest/DAOTest/PersonDAOTest.java
+++ src/test/java/modelTest/DAOTest/PersonDAOTest.java
@@ -2,11 +2,14 @@ package modelTest.DAOTest;
 
 
 
+import org.bihe.DAO.EstateDAO;
 import org.bihe.DAO.PersonDAO;
+import org.bihe.DAO.PlayerDAO;
 import org.bihe.model.Person;
 import org.bihe.util.FileService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -17,53 +20,73 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PersonDAOTest {
     private static PersonDAO personDAO;
     private static Map<String, Person> testPersons;
+    private static Map<String, Person> testPlayers;
     private static Person person;
+    private PlayerDAO playerDao;
 
 
-    @BeforeAll
-    public static void setUp() {
+    @BeforeEach
+    public void setUp() {
 
         personDAO = PersonDAO.getPersonDAO();
         personDAO.getPersons();
         testPersons = new HashMap<>();
         person = new Person("eve", "980428");
-        personDAO.removePerson(person);
+
     }
 
     @Test
+    public void testRemovePerson() {
+        personDAO.removePerson(person);
+        testPersons = personDAO.getPersons();
+        assert testPersons.containsKey("eve") == false;
+    }
+    @Test
     public void testAddPerson() {
+        personDAO.removePerson(person);
         assertTrue(personDAO.addPerson(person));
         testPersons = personDAO.getPersons();
         assert testPersons.containsKey("eve") == true;
     }
 
     @Test
-    public void testRemovePerson() {
-        assertTrue(personDAO.addPerson(person));
-        personDAO.removePerson(person);
-        testPersons = personDAO.getPersons();
-        assert testPersons.containsKey("eve") == false;
-    }
-
-    @Test
     public void testSetSignin(){
-        personDAO.setUserThatSignIn("testUser");
+        personDAO.setUserThatSignIn("eve");
         String testPerson = personDAO.getUserThatSignIn();
-        assert testPerson == "testUser";
+        assert testPerson == "eve";
     }
 
     @Test
     public void testGetThePerson(){
-        personDAO.setUserThatSignIn("testUser");
+        personDAO.addPerson(person);
+        personDAO.setUserThatSignIn("eve");
         Person testPerson = personDAO.getThePerson();
-        assert testPerson.getUserName().equals("testUser");
+        assert testPerson.getUserName().equals("eve");
     }
 
     @Test
     public void testGetPersons(){
         testPersons = personDAO.getPersons();
-        assert testPersons.containsKey("testUser") == true;
+        assert testPersons.containsKey("eve") == true;
 
+    }
+
+    @Test
+    public void testCheckForPersons(){
+
+        assert personDAO.checkForPerson("testUser","password");
+
+    }
+
+    @Test
+    public void testChangePersonForPlayerDao(){
+        testPlayers = PlayerDAO.getPlayerDAO().getPlayers();
+        personDAO.setUserThatSignIn("eve");
+        person.setPassword("12345");
+        personDAO.changePerson(person);
+        testPersons = personDAO.getPersons();
+        Person newPerson = testPersons.get("eve");
+        assert newPerson.getPassword().equals("12345");
     }
 
 
