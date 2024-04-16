b445878c7cfa74ccca681a94e312a8ece8b8f7df
Emily Berger <heyitsM@users.noreply.github.com>
2024-04-16 15:09:48 -0400

    added file cleanup

--- src/test/java/utilTest/FileServiceTest.java
+++ src/test/java/utilTest/FileServiceTest.java
@@ -2,13 +2,21 @@ package utilTest;
 
 import org.bihe.util.FileService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+* Important: delete newread text file before running tests for all to pass
+* */
+
 public class FileServiceTest {
-    String existingFilepath = "./src/test/java/utilTest/fileservice.txt";
-    String newFilePath = "./src/test/java/utilTest/newfileservice.txt";
-    String neverExistingFilePath = "./src/test/java/utilTest/newread.txt";
+    static String existingFilepath = "./src/test/java/utilTest/fileservice.txt";
+    static String newFilePath = "./src/test/java/utilTest/newfileservice.txt";
+    static String neverExistingFilePath = "./src/test/java/utilTest/newread.txt";
 
     @Test
     public void testBasicFileWriteToExistingFile() {
@@ -28,5 +36,14 @@ public class FileServiceTest {
     @Test
     public void testFileReadNullOnNewFile() {
         assertNull(FileService.readObj(neverExistingFilePath));
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        File file = new File(newFilePath);
+
+        if (!file.delete()) {
+            System.out.println("Issue deleting");
+        }
     }
 }
