afd5274a22a700e02bafa479a83b14e4752b21cf
ZheyueZhao <zheyuezhao@gmail.com>
2024-04-19 16:44:03 -0400

    add another test for piece worker

--- src/test/java/uiTest/PieceWorkerTest.java
+++ src/test/java/uiTest/PieceWorkerTest.java
@@ -1,16 +1,18 @@
 package uiTest;
-import org.bihe.ui.BuyStreetDialog;
+import org.bihe.network.client.Client;
+import org.bihe.ui.GUIManager;
+import org.bihe.ui.GamePanel;
 import org.bihe.ui.Piece;
 import org.bihe.ui.PieceWorker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class PieceWorkerTest {
@@ -39,8 +41,6 @@ public class PieceWorkerTest {
 
         assert piece.getX() == -3;
         verify(component, times(3)).repaint();
-
-
     }
 
     @Test
@@ -56,8 +56,35 @@ public class PieceWorkerTest {
 
         assert piece.getX() == 0;
         verify(component, times(3)).repaint();
+    }
 
+    @Test
+    public void testPanel10Move() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        piece.setMove(true);
+        piece.setY(3);
+        piece.setPanelNumber(10);
 
+        JPanel newPanel = new JPanel();
+
+        newPanel.setPreferredSize(new Dimension(640, 480));
+
+        try(MockedStatic mockedGUIManager = mockStatic(GUIManager.class)) {
+
+            Dimension dm = new Dimension(640, 400);
+            GamePanel gamePanel = mock(GamePanel.class);
+            //gamePanel.addAllPieces();
+            //gamePanel.getPanelSize(10);
+            mockedGUIManager.when(GUIManager::getGamePanel).thenReturn(gamePanel);
+
+            when(gamePanel.getPanelSize(anyInt())).thenReturn(dm);
+            PieceWorker pieceWorkerTest = new PieceWorker(component, piece, 2, true);
+            Class<?> pieceWorkerTestClass = pieceWorkerTest.getClass();
+            Method panelNo10MoveMethod = pieceWorkerTestClass.getDeclaredMethod("panelNo10Move");
+            panelNo10MoveMethod.setAccessible(true);
+            panelNo10MoveMethod.invoke(pieceWorkerTest);
+        }
+        assert piece.getY() == 0;
+        verify(component, times(3)).repaint();
     }
 
 }
