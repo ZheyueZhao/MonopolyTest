aff2a075d9469353420fc20f3a9a9f02ee12443b
Emily Berger <heyitsM@users.noreply.github.com>
2024-04-21 18:20:08 -0400

    removed EstateDAO modification

--- src/test/java/modelTest/RequestTest.java
+++ src/test/java/modelTest/RequestTest.java
@@ -12,8 +12,6 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
-import javax.swing.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -53,7 +51,7 @@ public class RequestTest {
     public void testPurchaseNoMoney() {
         response = 0;
         RequestDialog mockRequestDialog = mock(RequestDialog.class);
-        try(MockedStatic mockedGUIManager = mockStatic(GUIManager.class)){
+        try(MockedStatic<GUIManager> mockedGUIManager = mockStatic(GUIManager.class)){
                 mockedGUIManager.when(GUIManager::getRequestDialog).thenReturn(mockRequestDialog);
                 testRequest.analyseRequest();
                 mockedGUIManager.verify(GUIManager::getRequestDialog, times(2));
@@ -82,7 +80,7 @@ public class RequestTest {
 
     @Test
     public void testRequestWith1Response() {
-        testRequest = new Request(giveEstates, recieveEstates, 50, 50, "testUser", "testUser");
+        testRequest = new Request(new ArrayList<Estate>(), new ArrayList<Estate>(), 50, 50, "testUser", "testUser");
         testRequest.setResponse(1);
         PersonDAO.getPersonDAO().getPersons();
         PersonDAO.getPersonDAO().setUserThatSignIn("testUser");
@@ -90,7 +88,7 @@ public class RequestTest {
 
         Client sampleMock = mock(Client.class);
 
-        try(MockedStatic mockedClient = mockStatic(Client.class)) {
+        try(MockedStatic<Client> mockedClient = mockStatic(Client.class)) {
             mockedClient.when(Client::getClient).thenReturn(sampleMock);
             doNothing().when(sampleMock).sendObject(any());
             testRequest.analyseRequest();
@@ -99,7 +97,7 @@ public class RequestTest {
 
     @Test
     public void testRequestWithNegativeOneResponse() {
-        testRequest = new Request(giveEstates, recieveEstates, 50, 50, "testUser", "testUser");
+        testRequest = new Request(new ArrayList<Estate>(), new ArrayList<Estate>(), 50, 50, "testUser", "testUser");
         testRequest.setResponse(-1);
         testRequest.analyseRequest();
     }
