94ee138eab2d08df535e489b3d882ef9420e7f7a
Emily Berger <heyitsM@users.noreply.github.com>
2024-04-21 00:47:55 -0400

    full coverage on RailRoad Test

--- src/test/java/modelTest/RailRoadTest.java
+++ src/test/java/modelTest/RailRoadTest.java
@@ -34,6 +34,18 @@ public class RailRoadTest {
     }
 
     @Test
+    public void testOtherConstructorAndSetters() {
+        railRoad = new RailRoad("Railroad", 3, 200, 50, 100, 150);
+        railRoad.setRentForTwo(50);
+        railRoad.setRentForThree(100);
+        railRoad.setRentForfour(150);
+
+        assertEquals(50, railRoad.getRentForTwo());
+        assertEquals(100, railRoad.getRentForThree());
+        assertEquals(150, railRoad.getRentForfour());
+    }
+
+    @Test
     public void testRent() {
         railRoad = new RailRoad("Railroad", 1, 200, 25, 100, 120, 50, 100, 200);
         estateDao = EstateDAO.getEstateDAO();
@@ -63,7 +75,101 @@ public class RailRoadTest {
         System.out.println(estates.get(5).getOwner());
         // Test rent calculation with different number of owned railroads
         assertEquals(200, railRoad.rent()); // No owned railroads
+    }
+
+    @Test
+    public void testRentOwnsThree() {
+        railRoad = new RailRoad("Railroad", 1, 200, 25, 100, 120, 50, 100, 200);
+
+        estateDao = EstateDAO.getEstateDAO();
+
+        estate = estateDao.getOneEstate(5);
+        estate.setOwner("Owner");
+        estateDao.changeEstate(estate);
+
+        estate = estateDao.getOneEstate(15);
+        estate.setOwner("NotOwner");
+        estateDao.changeEstate(estate);
+
+        estate = estateDao.getOneEstate(25);
+        estate.setOwner("NotOwner");
+        estateDao.changeEstate(estate);
+
+        estate = estateDao.getOneEstate(35);
+        estate.setOwner("NotOwner");
+        estateDao.changeEstate(estate);
+
+        EstateDAO estateDAO = mock(EstateDAO.class);
+        HashMap<Integer, Estate> estates = new HashMap<>();
+        railRoad.setOwner("NotOwner");
+        estates.put(15, railRoad);
+        when(estateDAO.getEstates()).thenReturn(estates);
 
 
+        assertEquals(100, railRoad.rent()); // Three owned railroads
+    }
+
+    @Test
+    public void testRentOwnsTwo() {
+        railRoad = new RailRoad("Railroad", 1, 200, 25, 100, 120, 50, 100, 200);
+
+        estateDao = EstateDAO.getEstateDAO();
+
+        estate = estateDao.getOneEstate(5);
+        estate.setOwner("Owner");
+        estateDao.changeEstate(estate);
+
+        estate = estateDao.getOneEstate(15);
+        estate.setOwner("NotOwner");
+        estateDao.changeEstate(estate);
+
+        estate = estateDao.getOneEstate(25);
+        estate.setOwner("Owner");
+        estateDao.changeEstate(estate);
+
+        estate = estateDao.getOneEstate(35);
+        estate.setOwner("NotOwner");
+        estateDao.changeEstate(estate);
+
+        EstateDAO estateDAO = mock(EstateDAO.class);
+        HashMap<Integer, Estate> estates = new HashMap<>();
+        railRoad.setOwner("NotOwner");
+        estates.put(15, railRoad);
+        when(estateDAO.getEstates()).thenReturn(estates);
+
+
+        assertEquals(50, railRoad.rent()); // Three owned railroads
+    }
+
+    @Test
+    public void testRentOwnsOne() {
+        railRoad = new RailRoad("Railroad", 1, 200, 25, 100, 120, 50, 100, 200);
+
+        estateDao = EstateDAO.getEstateDAO();
+
+        estate = estateDao.getOneEstate(5);
+        estate.setOwner("NotOwner");
+        estateDao.changeEstate(estate);
+
+        estate = estateDao.getOneEstate(15);
+        estate.setOwner("Owner");
+        estateDao.changeEstate(estate);
+
+        estate = estateDao.getOneEstate(25);
+        estate.setOwner("Owner");
+        estateDao.changeEstate(estate);
+
+        estate = estateDao.getOneEstate(35);
+        estate.setOwner("Owner");
+        estateDao.changeEstate(estate);
+
+        EstateDAO estateDAO = mock(EstateDAO.class);
+        HashMap<Integer, Estate> estates = new HashMap<>();
+        railRoad.setOwner("NotOwner");
+        estates.put(5, railRoad);
+        when(estateDAO.getEstates()).thenReturn(estates);
+
+
+        assertEquals(25, railRoad.rent()); // One owned railroads
     }
 }
