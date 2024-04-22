e7766f71a8e8297fae12307ddeb132082affdd25
ZheyueZhao <zheyuezhao@gmail.com>
2024-04-21 23:04:45 -0400

    dice test

---
+++ src/test/java/uiTest/actionPanelTest/DicePanelTest.java
@@ -0,0 +1,51 @@
+package uiTest.actionPanelTest;
+
+import org.bihe.ui.Piece;
+import org.bihe.ui.actionPanel.DicePanel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DicePanelTest {
+    DicePanel dicePanel;
+    @BeforeEach
+    public void setUp() {
+        // You may need to mock the Image object
+        dicePanel = new DicePanel();
+
+    }
+
+    @Test
+    public void randomDistributionTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        Class<?> pieceWorkerTestClass = dicePanel.getClass();
+        Method makeRandomMethod = pieceWorkerTestClass.getDeclaredMethod("makeRandom");
+        makeRandomMethod.setAccessible(true);
+        makeRandomMethod.invoke(dicePanel);
+
+
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for (int i = 0; i < 30000; i++) {
+
+            int randomNumber = (int) makeRandomMethod.invoke(dicePanel);
+            frequencyMap.put(randomNumber, frequencyMap.getOrDefault(randomNumber, 0) + 1);
+        }
+
+        for (int number = 1; number < 7; number++) {
+            int frequency = frequencyMap.getOrDefault(number, 0);
+            for(int num=1; num<7;num++){
+                int alterFrequency = frequencyMap.getOrDefault(number, 0);
+                assert frequency <= alterFrequency*1.5 && frequency >= alterFrequency*0.5;
+            }
+
+        }
+
+    }
+}
