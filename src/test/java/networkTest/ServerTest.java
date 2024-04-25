5dc45ea89419f6773fd3f9065167cbfcdbf5ed27
Emily Berger <heyitsM@users.noreply.github.com>
2024-04-25 01:45:08 -0400

    did some more work on servertest

--- src/test/java/networkTest/ServerTest.java
+++ src/test/java/networkTest/ServerTest.java
@@ -7,16 +7,28 @@ import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
 import java.net.ServerSocket;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 public class ServerTest {
     public Server serverSpy;
-    public int port = 6666;
+    public int port = 3000;
 
     @Test
-    public void testing() throws Exception {
+    public void testGetServerWhenInstanceNull() {
+        assertNotNull(Server.getServer());
+    }
+
+    @Test
+    //Depends on configuration, whether or not this should be thrown - throughout the app, many errors are thrown to the
+    //JOptionPane, but they didn't catch all errors - and this is one that is instead thrown to console
+    public void testSendObjectWhenNotConfigured() {
+        assertThrows(IndexOutOfBoundsException.class, () -> Server.getServer().sendObject(0, "Hello world"));
+    }
+
+    @Test
+    public void testingRunServerBaseWorks() throws Exception {
         ServerSocket ss = new ServerSocket(port);
 
         try(MockedConstruction<ServerSocket> mockSocket = Mockito.mockConstruction(ServerSocket.class, (mock, context)-> {
@@ -29,4 +41,5 @@ public class ServerTest {
             assertDoesNotThrow(()-> serverSpy.runServer(port, 1));
         }
     }
+
 }
